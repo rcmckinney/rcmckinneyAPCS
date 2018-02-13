bca7293788f1e2796c13544947dc51c7bcf01802
rcmckinney <ryan.c.mckinney15574@gmail.com>
2018-02-12 17:16:32 -0800

    adds functions folder

---
+++ functions/functions.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Functions{
+    // public static void main(String args[]) {
+    //     System.out.print("Enter a string: ");
+    //     Scanner scan = new Scanner(System.in);
+    //     if(isValidString(scan)) {
+    //         System.out.println("String Success!");
+    //     }     
+        
+    //     else {
+    //         System.out.println("String failure!" );
+    //     }
+
+    // }
+    // public static Boolean isValidString(Scanner scan) {
+    //     try {
+    //         String s = scan.nextLine();
+    //     }
+    //     catch(Exception e) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+
+        public static void main(String args[]) {
+        System.out.print("Enter an int: ");
+        Scanner scan = new Scanner(System.in);
+        if(isValidInt(scan)) {
+            System.out.println("Int Success!");
+        }     
+        
+        else {
+            System.out.println("Int failure!" );
+        }
+
+    }
+    public static Boolean isValidInt(Scanner scan) {
+        try {
+            String s = scan.nextLine();
+        }
+        catch(Exception e) {
+            return false;
+        }
+        return true;
+    }
+}

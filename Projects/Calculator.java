cd747cb2da7a56c6cf2cd63b3668d36a2d5c3c1c
rcmckinney <ryan.c.mckinney15574@gmail.com>
2018-02-20 17:06:17 -0800

    adds code for the finished loan calculator

--- Projects/Calculator.java
+++ Projects/Calculator.java
@@ -5,6 +5,8 @@ public class Calculator{
     public static double loanAmt = -1;
     public static int loanTerm = -1;
     public static double interestRate = -1;
+    public static double interest = 0;
+    public static int monthsRemaining = -1;
 
     public static void main(String[] args) {
         //Read in the loan calculation mode
@@ -27,6 +29,9 @@ public class Calculator{
         }
         if(mode == 2) {
             compoundNoPay();
+        }
+        if(mode == 3){
+            compoundWithPay();
         }
     }
 
@@ -123,5 +128,75 @@ public class Calculator{
                 }
             }
         }
+        monthsRemaining = loanTerm * 12;
+        while(monthsRemaining > 0){
+            interest = interest + (loanAmt + interest) * (interestRate/1200);
+            monthsRemaining -= 1;
+        }
+        double totalPayment = interest + loanAmt;
+             System.out.println(
+            "Original loan amount: $" + loanAmt);
+        System.out.println(
+            "Loan Term: " + loanTerm + " years");
+        System.out.println(
+            "Interest Rate: " + interestRate + " %");
+        System.out.println(
+            "Interest to be paid: $" + interest);
+        System.out.println(
+            "Total amount of loan: $" + totalPayment);
+    }
+    public static void compoundWithPay() {
+        while(true) {
+            System.out.print("Enter the amount of your loan: ");
+            Scanner scan;
+            scan = new Scanner(System.in);
+            if(scan.hasNextDouble()) {
+                loanAmt = scan.nextDouble();
+                if (loanAmt > 0) {
+                    break;
+                }
+            }
+        }
+               while(true) {
+            System.out.print("Enter the term of your loan (years): ");
+            Scanner scan;
+            scan = new Scanner(System.in);
+            if(scan.hasNextInt()) {
+                loanTerm = scan.nextInt();
+                if (loanTerm > 0) {
+                    break;
+                }
+            }
+        }
+                 while(true){
+            System.out.print("Enter the interest on your loan (%): ");
+            Scanner scan;
+            scan = new Scanner(System.in);
+            if(scan.hasNextDouble()){
+                interestRate = scan.nextDouble();
+                if(interestRate > 0){
+                    break;
+                }
+            }
+        } 
+        double ratePerMonth = interestRate / 1200;
+        double monthlyPayment = loanAmt * (ratePerMonth / (1 - Math.pow((1 + ratePerMonth), (loanTerm * -12))));
+        double moneyOwed = loanAmt;
+        double interestTwo = 0;
+        while(moneyOwed > 0) {
+            interest = moneyOwed * ratePerMonth;
+            moneyOwed = interest + moneyOwed - monthlyPayment;
+            interestTwo = interestTwo + interest;
+        }
+        double totalAmountToBePaid = interestTwo + loanAmt;
+        double finalMonthPayment = monthlyPayment + totalAmountToBePaid - (monthlyPayment * 12 * loanTerm);
+        System.out.println("Original loan amount: $" + loanAmt);
+        System.out.println("Interst rate: " + interestRate + "%");
+        System.out.println("Loan Term: " + loanTerm + " years");
+        System.out.println("Monthly payment: $" + monthlyPayment);
+        System.out.println("Final monthly payment: $" + finalMonthPayment);
+        System.out.println("Total amount paid: $" + totalAmountToBePaid);
+        System.out.println("Interest paid: $" + interestTwo);
+        
     }
 }

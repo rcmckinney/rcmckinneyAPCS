a807f6e73ef2365957013fb850482b0d05d80578
Ryan McKinney <McKin.Ryan2018@ssusd.org>
2018-02-22 07:36:54 -0800

    adds the loops assignment

---
+++ Assignments/Loops/Loops.java
@@ -0,0 +1,74 @@
+public class Loops{
+    public static void main(String[] args) {
+        // int n = 0;
+        // while(n < 100) {    // This will make an infinite loop
+        //     System.out.println(n);
+        //     n *= 5;
+        // }
+      
+        // int n = 19;
+        //     while(n % 2 == 0) { // This prints nothing
+        //         System.out.println(n);
+        //         n--;        
+        // }
+       
+        // int n = 1;
+        // while(n < 100) {
+        //     System.out.println(n);
+        //     n += n;        
+        // }
+       
+        // Boolean shouldLoop = true;
+        // int n = 0;
+        // while(shouldLoop) {
+        //     if(n == 20) {
+        //         shouldLoop = false;
+        //     }
+        //     System.out.println(n);
+        //     n++;
+        // }
+      
+        // int n = 0;
+        // while(true) {
+        //     System.out.println("Hello World");
+        //     n++;
+        //     if(n == 10){
+        //         break;
+        //     }
+        // }
+       
+        // int n = 1;
+        // while(n < 10){ // The original code did not change (n) in any way
+        //     System.out.println(n);
+        //     n++;
+        // }
+       
+        // Boolean shouldLoop = true;
+        // int n = 1;
+        // while(shouldLoop) { // The original code did not have a break statement
+        //     if(n == 100) {
+        //         System.out.println("100 loop passes occured.");
+        //         break;
+        //     }
+        //     n++;
+        // }
+
+        // int n = 100; // Counts from 100 to -100 by 1's
+        // while(n >= -100) {
+        //     System.out.println(n);
+        //     n--;
+        // }
+
+        // int n = 3;  //counts from 3 to 120 by 3's
+        // while(n <= 120) {
+        //     System.out.println(n);
+        //     n += 3;
+        // }
+
+        char aplha = 'a'; // Need to fix, won't compile
+        while(alpha <= z){
+            System.out.println(alpha);
+            alpha++;
+        }
+    }
+}

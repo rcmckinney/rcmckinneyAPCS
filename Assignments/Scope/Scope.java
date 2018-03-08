da043a97d795b79d302e2a0b9fdc99bce9cfd98f
Ryan McKinney <McKin.Ryan2018@ssusd.org>
2018-03-08 07:36:59 -0800

    adds the Scope assignment

---
+++ Assignments/Scope/Scope.java
@@ -0,0 +1,54 @@
+// class Scope {
+//     // public static String message = "Hello";
+//     // public static Boolean shouldLoop = false;
+
+//     public static void main(String args[]) {
+//     //     int i;
+//     //     i = 0;
+//     //     int n = i * 2;
+//     //     while(i <= 10) {
+//     //         n = i * 2;
+//     //         System.out.println(n);
+//     //         i++;
+//     //     }
+
+//     //     if(n > 10) {
+//     //         String message = "Hello World";
+//     //         System.out.println(message);
+//     //     }
+
+        
+//     //     while(shouldLoop) {
+//     //         if(i < 10) {
+//     //             System.out.println(message);                    
+//     //         } else {
+//     //             Boolean shouldLoop = false;
+//     //         }
+//     //         i++;
+//     //     }
+    
+//     String message = "Hello World";
+//     int i;
+//     for(i = 0; i < message.length(); i++) {
+//         if(message.charAt(i) == 'W') {
+//             break;
+//         }
+//     }
+//     System.out.println("W found at index " + i);
+//     }
+// }
+
+class Powers {
+    public static final int n = 32;
+    public static int base = 2;
+
+    public static void main(String[] args) {
+        for(int i = 0; i <= n; i++) {
+            power();
+        }
+    }
+
+    public static void power() {
+        return -1;
+    }
+}

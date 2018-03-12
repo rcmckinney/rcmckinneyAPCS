283bf6e03e4e1e080f17f886b33650803ee86938
Ryan McKinney <McKin.Ryan2018@ssusd.org>
2018-03-12 07:36:22 -0700

    adds the Iterating Data assignment

---
+++ Assignments/Powers/Powers.java
@@ -0,0 +1,9 @@
+class Powers {
+    public static void main(String[] args) {
+        int[] nums = new int[201];
+
+        for(int i = -100; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}

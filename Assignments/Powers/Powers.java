0c74e9b6208e78cfea9f1de74f9933b1886644ab
Ryan McKinney <McKin.Ryan2018@ssusd.org>
2018-03-13 07:20:34 -0700

    adds some code to the iteration assignment

--- Assignments/Powers/Powers.java
+++ Assignments/Powers/Powers.java
@@ -2,8 +2,9 @@ class Powers {
     public static void main(String[] args) {
         int[] nums = new int[201];
 
-        for(int i = -100; i <= 100; i++) {
-            System.out.println(i);
+        nums[0] = -100;
+        for(int value : nums) {
+            System.out.println(nums);
         }
     }
 }

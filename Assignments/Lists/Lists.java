d73ab9ccbb654878ead959270be10141093106be
Ryan McKinney <McKin.Ryan2018@ssusd.org>
2018-03-15 07:39:54 -0700

    adds code to the Lists assignment

--- Assignments/Lists/Lists.java
+++ Assignments/Lists/Lists.java
@@ -1,7 +1,38 @@
 import java.util.*;
 
 class Lists {
+    public static final int Add = 1;
+    public static final int Remove = 2;
+    public static final int Quit = 3;
     public static void main(String[] args) {
+        ArrayList<Integer> todoList = new ArrayList<Integer>();
+        printStatement();
+        if(scan.hasNextInt()) {
+            mode = scan.NextInt();
+            if(mode == Add) {
+                Add();
+            }
+            else if(mode == Remove) {
+                Remove();
+            }
+            else if(mode == Quit) {
+                System.out.println("Goodbye");
+                break;
+            }
+            else {
+                continue;
+            }
+        }
+    }
+    public static void printStatement() {
+        System.out.print(
+            "What would you like to do?\n"
+            +"\t(1)Add something to the list\n"
+            +"\t(2)Remove something from the list\n"
+            +"\t(3)Quit\n"
+        );
+    }
+    public static void Add() {
         
     }
 }
